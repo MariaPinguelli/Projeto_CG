42c185f5312a0beaa33a8d21ef9194615a02a57d
Eduarda <eduarda_smartins@outlook.com>
2022-12-08 17:48:35 -0300

    Iniciando a disposição da imagem de fundo

--- main.c
+++ main.c
@@ -1,22 +1,42 @@
+#include <GL/glew.h>
 #include <GL/glut.h>
 #include "main.h"
+#include "./textures-hands-on/stb-master/stb_image.h"
+#include "./textures-hands-on/stb-master/stb_image_write.h"
 
-void display(void){
+#define STB_IMAGE_IMPLEMENTATION
+
+// Variáveis para criação do fundo
+int altura = 800, largura = 800;
+
+void background()
+{
+    int width, height, channels;
+    unsigned char *img = stbi_load("./images/background.png", &width, &height, &channels, 0);
+     stbi_write_jpg("./images/background.png", width, height, channels, img, 100);
+}
+
+void display(void)
+{
     pipe(0.4, -0.4);
 }
 
-void setup() {
+void setup()
+{
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    background();
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
     glutInitWindowSize(800, 800);
-    glutCreateWindow("Hello World");
+    glutCreateWindow("Flappy Bird");
 
     setup();
     glutDisplayFunc(display);
+    // glutDisplayFunc(background);
 
     glutMainLoop();
     return 0;
