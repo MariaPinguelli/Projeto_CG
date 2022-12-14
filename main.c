215fc00863d33beb27c06322339c52088f2fb71d
MariaPinguelli <mariaferpinguelli@hotmail.com>
2022-12-14 15:22:37 -0300

    teste

--- main.c
+++ main.c
@@ -1,93 +1,77 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
-#define WIDTH 400
-#define HEIGHT 400
-#define POS_X 350
-#define POS_Y 60
-#define MAX 24
+void keyPressed_special(int key, int x, int y);
 
-// Comando para Compilar: gcc main.c -o main -lglut -lGLU -lGL
+float angulo_x = 0, angulo_y = 0, escala = 1;
 
-int mapa[MAX][MAX] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0},
-    {0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+int init(void){
+	glClearColor(0, 0, 0, 0);		// define a cor de fundo
+	glEnable(GL_DEPTH_TEST);		// remoção de superfície oculta
+	// define que a matriz é a de projeção
+	glMatrixMode(GL_PROJECTION);
 
-int init()
-{
-    for (int i = 0; i < MAX; i++)
-    {
-        for (int j = 0; j < MAX; j++)
-        {
-            glColor3f(1, 0, 0);
-            glPushMatrix();
-            glTranslatef(5, 2, 1);
-            glutSolidCube(1);
-            glPopMatrix();
-        }
+	glLoadIdentity();				// carrega a matriz identidade
+	glOrtho(-5, 5, -5, 5, -5, 5);	// define uma projeção ortográfica
+}
+
+
+void display(void){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);	// define que a matriz é a de modelo
+	glLoadIdentity();				// carrega a matriz identidade
+
+	glPushMatrix();
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef(3, 0, 0);
+    glRotatef(angulo_x, 1, 0, 0);
+    glRotatef(angulo_y, 0, 1, 0);
+    glutWireSphere(1.0, 20, 20);
+    glPopMatrix();
+
+    glColor3f(1.0, 1.0, 0.0);
+    glTranslatef(-3, 0, 0);
+    glRotatef(angulo_x, 1, 0, 0);
+    glRotatef(angulo_y, 0, 1, 0);
+    glutWireSphere(1.0, 20, 20);
+
+	glFlush();						// desenha os comandos não executados
+}
+
+void keyPressed_special(int key, int x, int y){
+    if (key == GLUT_KEY_UP){
+        angulo_x += 0.5;
     }
-
-    return 0;
+    if (key == GLUT_KEY_DOWN){
+        angulo_x -= 0.5;
+    }
+    if (key == GLUT_KEY_LEFT){
+        angulo_y -= 0.5;
+    }
+    if (key == GLUT_KEY_RIGHT){
+        angulo_y += 0.5;
+    }
+    if (key == GLUT_KEY_PAGE_DOWN){
+        escala -= 0.1;
+    }
+    if (key == GLUT_KEY_PAGE_UP){
+        escala += 0.1;
+    }
+    glutPostRedisplay();
 }
 
-void display(){
-    glColor3f(1, 0, 0);
-    glPushMatrix();
-    glTranslatef(5, 2, 1);
-    glutSolidCube(1);
-    glPopMatrix();
+int main(int argc, char** argv){
+    glutInit(&argc,argv);			//inicializa o GLUT
+	//configura o modo de display
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowPosition(200,0);	//seta a posição inicial da janela
+    glutInitWindowSize(800,800); 	//largura e altura da janela
+    glutCreateWindow("Labirinto");			//cria a janela de exibição
 
-    glPushMatrix();
-    glTranslatef(10, 8, 0);
-    glutSolidCube(1);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(3, 7, 9);
-    glutSolidCube(1);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(1, 4, 6);
-    glutSolidCube(1);
-    glPopMatrix();
-}
-
-int main(int argc, char **argv){
-    glutInit(&argc, argv); // Iniciar o OpenGL
-    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    // glutInitWindowPosition(POS_X, POS_Y);
-    glutCreateWindow("Labyrinth");
-    init();
-    // glutSpecialFunc(Special);
-    // glutSpecialUpFunc(Special_Up);
-    // glutKeyboardFunc(Keyboard);
-    // glutTimerFunc(0, Timer, 0);
+    init();							//executa função de inicialização
+    glutSpecialFunc(keyPressed_special);
     glutDisplayFunc(display);
-    glutMainLoop();
-
+    glutMainLoop();					//mostre tudo e espere
     return 0;
 }
+
